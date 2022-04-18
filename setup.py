a9a3600e1636705fb22aaafa7c140930641765ff
ramnes <contact@ramnes.eu>
2022-04-18 11:07:46 +0200

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="sftpgo-client",
-    version="0.3.0",
+    version="0.3.1",
     url="https://github.com/ramnes/sftpgo-client",
     author="Guillaume Gelin",
     author_email="contact@ramnes.eu",
